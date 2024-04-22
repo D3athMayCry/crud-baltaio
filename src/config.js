dd534ef761f06995191e0d0a0298cd37a66370ae
X0rbit <109915695+X0rb1t@users.noreply.github.com>
2024-04-22 12:25:35 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 
 module.exports = {
-    connectionString: 'mongodb+srv://john:john@cluster0.5kpojv3.mongodb.net/',
+    connectionString: 'SUA STRING DE CONEXÃO!',
     sendgridKey: 'TBD',
     containerConnectionString: 'TBD'
 };
